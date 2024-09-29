7b445f8f7f69d43a7cca4ff1a5500879a91325b1
smpark <smpark@pnpsecure.com>
2024-09-29 23:44:51 +0900

    lifetime parameter

--- lifetime/src/main.rs
+++ lifetime/src/main.rs
@@ -1,3 +1,17 @@
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if y.len() < x.len() {
+        x
+    } else {
+        y
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        let result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {}", result);
+    }
 }
