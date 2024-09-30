710624c6d463880caa6957a6c375bf5961af6596
smpark <smpark@pnpsecure.com>
2024-09-30 22:46:03 +0900

    lifetime struct

--- lifetime/src/main.rs
+++ lifetime/src/main.rs
@@ -1,3 +1,7 @@
+struct ImportantExcerpt<'a> {
+	part: &'a str,
+}
+
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if y.len() < x.len() {
         x
@@ -14,4 +18,22 @@ fn main() {
         let result = longest(string1.as_str(), string2.as_str());
         println!("The longest string is {}", result);
     }
+
+	let novel = String::from("Call me Ishmael. Some years ago...");
+	let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+	let i = ImportantExcerpt {
+		part:first_sentence,
+	};
+	println!("{}", i.part);
+
+	// 라이프타임 오류
+	// let i;
+	// {
+	// 	let novel = String::from("Call me Ishmael. Some years ago...");
+	// 	let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+	// 	i = ImportantExcerpt {
+	// 		part:first_sentence,
+	// 	};
+	// }
+	// println!("{}", i.part);
 }
