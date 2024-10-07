3f33d11f4084cba1e6e7d669aa11532f4291598c
smpark <psm345601@naver.com>
2024-10-07 20:21:18 +0900

    리팩터링

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,32 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    // new()라는 네이밍은 실패하지 않을 것으로 예상하기 때문에 실패할 가능성이 있는 build가 적절
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        // String 타입의 clone()은 참조자를 젖아하는 것보다 시간과 메모리를 더 소모하지만,
+        // 라이프타임 관리를 할 필요가 없어 코드를 매우 직관적으로 만듦
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    // 반환값이 없으며 에러 처리만 필요함을 말함
+    Ok(())
+}
