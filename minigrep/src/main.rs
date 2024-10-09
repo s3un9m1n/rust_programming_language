e9454a48fb2fef103efced53c30a44c5f062c4c3
smpark <psm345601@naver.com>
2024-10-09 11:57:58 +0900

    표준에러로 에러 메시지 작성

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,11 +8,13 @@ fn main() {
     // dbg!(args);
 
     let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     // unwrap_or_else를 사용할 필요가 없는 것은 정상 케이스의 반환값이 없기 때문
     if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
