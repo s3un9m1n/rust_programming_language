bb28604a9c0a19ce874f1c87ac4e3d4817c72ba7
smpark <psm345601@naver.com>
2024-10-23 19:20:05 +0900

    공유메모리에 mutex 락을 이용해 동시성 제어

--- concurrency_shared_memory/src/main.rs
+++ concurrency_shared_memory/src/main.rs
@@ -1,4 +1,5 @@
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 fn main() {
     // Mutex<i32>는 i32가 아니므로 i32 값을 사용하기 위해서는 반드시 락을 얻어야 함
@@ -12,4 +13,27 @@ fn main() {
     }
 
     println!("m = {:?}", m);
+
+    // Mutex를 하나의 스레드로 전달하더라도 이후에 다른 스레드에서 접근하기 위함
+    // Arc<T>(아토믹 참조 카운팅)는 동시적 상황에서 안전하게 사용할 수 있는 Rc<T> 같은 타입/
+    // counter가 불변이지만 내부값의 가변 참조자를 가져올 수 있음
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            // mutex를 이용해 여러 스레드에서 값을 공유
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
