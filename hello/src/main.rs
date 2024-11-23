7468e585f9c4b30aabaf16e96277d296ba1cb4a6
s3un9m1n <psm345601@naver.com>
2024-11-23 10:00:07 +0900

    hello 웹서버 - 요청 유효성 검사

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -23,20 +23,32 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines() // 줄바꿈 발견 때마다 데이터 스트림을 분할(`Result<String, std::io::Error>` 반복자 반환)
-        .map(|result| result.unwrap()) // 각 `String` 뽑기 위해 `Result`를 `unwrap` (실제 프로덕션 코드에서는 적절한 에러 처리가 필요)
-        .take_while(|line| !line.is_empty()) // HTTP 종료 지점 확인 (HTTP는 연속된 두 번의 줄바꿈으로 HTTP 요청의 끝을 알림)
-        .collect(); // 라인들을 벡터에 수집
+    // HTTP 첫 번째 요청 라인만 확인
+    // 첫 번째 `unwrap`은 `Option`을 처리해 `next` 반복자의 아이템을 가져옴
+    // 두 번째 `unwrap`은 `Result`를 처리해 기존 `map`의 `unwrap`과 동일한 효과
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let length = contents.len();
+    
+        let response =
+            format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    
+        // `as_bytes()`: 문자열을 바이트로 변환
+        // `write_all()`: `&u[8]`을 받아 연결(`stream`)쪽으로 직접 보냄
+        stream.write_all(response.as_bytes()).unwrap();
+    }
+    else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let length = contents.len();
 
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
 
-    // `as_bytes()`: 문자열을 바이트로 변환
-    // `write_all()`: `&u[8]`을 받아 연결(`stream`)쪽으로 직접 보냄
-    stream.write_all(response.as_bytes()).unwrap();
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
