bf6651dc5c3ebc4ec9e510c00ccb3e2fe9c710bb
s3un9m1n <psm345601@naver.com>
2024-11-27 22:04:48 +0900

    hello 웹서버 - execute 구현

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -3,7 +3,10 @@ use std::{
     thread,
 };
 
-struct Job;
+/// `Job`
+/// `execute` 메서드에서 수신하는 클로저 타입을 갖도록,
+/// 트레이트 객체의 타입 별칭으로 변경
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 pub struct ThreadPool {
     // 각 worker에서 handle 인스턴스를 갖게 됨
@@ -50,6 +53,12 @@ impl ThreadPool {
     where
         F: FnOnce() + Send + 'static,
     {
+        // 클로저를 담는 `Box`(작업)를 채널로 보내기
+        let job = Box::new(f);
+        // `unwrap`: 전송 실패 시 종료
+        // 사실, `ThreadPool`을 사용할 경우 모든 스레드의 실행이 중지되지는 않을 것이지만,
+        // 컴파일러는 모르기 때문에 사용해야 함
+        self.sender.send(job).unwrap();
     }
 }
 
@@ -58,17 +67,29 @@ struct Worker {
     thread: thread::JoinHandle<()>, // 실제 스레드
 }
 
-
 impl Worker {
     // 구현 세부사항을 ThreadPool 등에게 알릴 필요가 없어, 비공개
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         // `Worker` 구조체는 `ThreadPool`에 보관된 대기열에서 실행할 코드를 가져와 자신의 스레드에서 실행하기를 원함
         // -> 스레드간 통신하는 방법을 사용
-        let thread = thread::spawn(|| {
+        // `move`: 소유권 이동
+        // `loop`: 작업 반복
+        let thread = thread::spawn(move || loop {
             // 클로저 내부에서 receiver 사용
-            receiver;
-        });
+            // 참조만 하는 것이 아니라 실제 작업을 수신 후 처리 필요
 
+            // `lock`: 뮤텍스 획득
+            // 다른 스레드에서 `lock` 획득 후 패닉에 빠질 경우,
+            // 뮤텍스가 독성(poisoned) 상태에 빠질 수 있어,
+            // `unwrap`을 통해 패닉 유도
+            // `recv`: 블록킹 방식으로 인해, 작업이 수신될 때까지 기다리게 될 것
+            // 이때 `Mutex<T>` 덕분에 한 번에 하나의 `Worker`만 작업 요청 가능
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {id} got a job; executing.");
+
+            job();
+        });
 
         Worker { id, thread }
     }
